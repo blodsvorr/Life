59538def5a099f23c23d0be8003bab34f00e576c
Alex <46936986+blodsvorr@users.noreply.github.com>
2022-06-05 14:40:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ function buildStateStr ( grid ) {
 	for ( var y in grid ) {
 		for ( var x in grid[y] ) {
 			if ( grid[y][x] === 1)
-				{ str += 'O' ;}
+				{ str += '$' ;}
 			else
 				{ str += '&nbsp;' ;}
 		}
